be9ef2c3c43a7d21dff92e6b13949e9fecbdd786
LiamHao <smile176223@gmail.com>
2022-02-02 10:55:41 +0800

    PoseDetect_MLKit

---
+++ Tireless/View Controller/PoseDetectViewController.swift
@@ -0,0 +1,431 @@
+//
+//  ViewController.swift
+//  Tireless
+//
+//  Created by Hao on 2022/2/1.
+//
+
+import AVFoundation
+import CoreVideo
+import MLKit
+
+class PoseDetectViewController: UIViewController {
+    
+    @IBOutlet weak var cameraPreView: UIView!
+    
+    private let detectors: [Detector] = [
+        .pose
+    ]
+    
+    private var currentDetector: Detector = .pose
+    private var isUsingFrontCamera = true
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    private lazy var captureSession = AVCaptureSession()
+    private lazy var sessionQueue = DispatchQueue(label: Constant.sessionQueueLabel)
+    private var lastFrame: CMSampleBuffer?
+    private var lastDetector: Detector?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        setUpPreviewOverlayView()
+        setUpAnnotationOverlayView()
+        setUpCaptureSessionOutput()
+        setUpCaptureSessionInput()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        startSession()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        stopSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        previewLayer.frame = cameraPreView.bounds
+
+    }
+
+    private lazy var previewOverlayView: UIImageView = {
+        precondition(isViewLoaded)
+        let previewOverlayView = UIImageView(frame: .zero)
+        previewOverlayView.contentMode = UIView.ContentMode.scaleAspectFill
+        previewOverlayView.translatesAutoresizingMaskIntoConstraints = false
+        return previewOverlayView
+    }()
+    
+    private lazy var annotationOverlayView: UIView = {
+        precondition(isViewLoaded)
+        let annotationOverlayView = UIView(frame: .zero)
+        annotationOverlayView.translatesAutoresizingMaskIntoConstraints = false
+        return annotationOverlayView
+    }()
+    
+    private var poseDetector: PoseDetector? = nil
+
+
+    private func detectPose(in image: VisionImage, width: CGFloat, height: CGFloat) {
+        if let poseDetector = self.poseDetector{
+            var poses: [Pose]
+            do {
+                poses = try poseDetector.results(in: image)
+                
+            } catch let error {
+                print("Failed to detect poses with error: \(error.localizedDescription).")
+                return
+                
+            }
+            weak var weakSelf = self
+            DispatchQueue.main.sync {
+                guard let strongSelf = weakSelf else {
+                    print("Self is nil!")
+                    return
+                }
+                strongSelf.updatePreviewOverlayViewWithLastFrame()
+                strongSelf.removeDetectionAnnotations()
+            }
+            guard !poses.isEmpty else {
+                //print("Pose detector returned no results.")
+                return
+            }
+            DispatchQueue.main.sync {
+                guard let strongSelf = weakSelf else {
+                    print("Self is nil!")
+                    return
+                }
+                // Pose detected. Currently, only single person detection is supported.
+                poses.forEach { pose in
+                    let poseOverlayView = UIUtilities.createPoseOverlayView(
+                        forPose: pose,
+                        inViewWithBounds: strongSelf.annotationOverlayView.bounds,
+                        lineWidth: Constant.lineWidth,
+                        dotRadius: Constant.smallDotRadius,
+                        positionTransformationClosure: { (position) -> CGPoint in
+                            return strongSelf.normalizedPoint(
+                                fromVisionPoint: position, width: width, height: height)
+                        }
+                    )
+                    strongSelf.annotationOverlayView.addSubview(poseOverlayView)
+                }
+
+
+            }
+        }
+    }
+    
+    private func setUpCaptureSessionOutput() {
+        weak var weakSelf = self
+        sessionQueue.async {
+            guard let strongSelf = weakSelf else {
+                print("Self is nil!")
+                return
+            }
+            strongSelf.captureSession.beginConfiguration()
+            // When performing latency tests to determine ideal capture settings,
+            // run the app in 'release' mode to get accurate performance metrics
+                    strongSelf.captureSession.sessionPreset = AVCaptureSession.Preset.vga640x480
+            
+            let output = AVCaptureVideoDataOutput()
+            output.videoSettings = [
+                (kCVPixelBufferPixelFormatTypeKey as String): kCVPixelFormatType_32BGRA
+            ]
+            output.alwaysDiscardsLateVideoFrames = true
+            let outputQueue = DispatchQueue(label: Constant.videoDataOutputQueueLabel)
+            output.setSampleBufferDelegate(strongSelf, queue: outputQueue)
+            guard strongSelf.captureSession.canAddOutput(output) else {
+                print("Failed to add capture session output.")
+                return
+            }
+            strongSelf.captureSession.addOutput(output)
+            strongSelf.captureSession.commitConfiguration()
+        }
+    }
+    
+    private func setUpCaptureSessionInput() {
+        weak var weakSelf = self
+        sessionQueue.async {
+            guard let strongSelf = weakSelf else {
+                print("Self is nil!")
+                return
+            }
+            let cameraPosition: AVCaptureDevice.Position = strongSelf.isUsingFrontCamera ? .front : .back
+            guard let device = strongSelf.captureDevice(forPosition: cameraPosition) else {
+                print("Failed to get capture device for camera position: \(cameraPosition)")
+                return
+            }
+            do {
+                strongSelf.captureSession.beginConfiguration()
+                let currentInputs = strongSelf.captureSession.inputs
+                for input in currentInputs {
+                    strongSelf.captureSession.removeInput(input)
+                }
+                
+                let input = try AVCaptureDeviceInput(device: device)
+                guard strongSelf.captureSession.canAddInput(input) else {
+                    print("Failed to add capture session input.")
+                    return
+                }
+                strongSelf.captureSession.addInput(input)
+                strongSelf.captureSession.commitConfiguration()
+            } catch {
+                print("Failed to create capture device input: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func startSession() {
+        weak var weakSelf = self
+        sessionQueue.async {
+            guard let strongSelf = weakSelf else {
+                print("Self is nil!")
+                return
+            }
+            strongSelf.captureSession.startRunning()
+        }
+    }
+    
+    private func stopSession() {
+        weak var weakSelf = self
+        sessionQueue.async {
+            guard let strongSelf = weakSelf else {
+                print("Self is nil!")
+                return
+            }
+            strongSelf.captureSession.stopRunning()
+        }
+    }
+    
+    private func setUpPreviewOverlayView() {
+        cameraPreView.addSubview(previewOverlayView)
+        NSLayoutConstraint.activate([
+            previewOverlayView.centerXAnchor.constraint(equalTo: cameraPreView.centerXAnchor),
+            previewOverlayView.centerYAnchor.constraint(equalTo: cameraPreView.centerYAnchor),
+            previewOverlayView.leadingAnchor.constraint(equalTo: cameraPreView.leadingAnchor),
+            previewOverlayView.trailingAnchor.constraint(equalTo: cameraPreView.trailingAnchor),
+            previewOverlayView.topAnchor.constraint(equalTo: cameraPreView.topAnchor),
+            previewOverlayView.bottomAnchor.constraint(equalTo: cameraPreView.bottomAnchor),
+            
+        ])
+    }
+    
+    private func setUpAnnotationOverlayView() {
+        cameraPreView.addSubview(annotationOverlayView)
+        NSLayoutConstraint.activate([
+            annotationOverlayView.topAnchor.constraint(equalTo: cameraPreView.topAnchor),
+            annotationOverlayView.leadingAnchor.constraint(equalTo: cameraPreView.leadingAnchor),
+            annotationOverlayView.trailingAnchor.constraint(equalTo: cameraPreView.trailingAnchor),
+            annotationOverlayView.bottomAnchor.constraint(equalTo: cameraPreView.bottomAnchor),
+        ])
+    }
+    
+    private func captureDevice(forPosition position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        if #available(iOS 10.0, *) {
+            let discoverySession = AVCaptureDevice.DiscoverySession(
+                deviceTypes: [.builtInWideAngleCamera],
+                mediaType: .video,
+                position: .unspecified
+            )
+            return discoverySession.devices.first { $0.position == position }
+        }
+        return nil
+    }
+    
+    private func updatePreviewOverlayViewWithLastFrame() {
+        guard let lastFrame = lastFrame,
+              let imageBuffer = CMSampleBufferGetImageBuffer(lastFrame)
+        else {
+            return
+        }
+        self.updatePreviewOverlayViewWithImageBuffer(imageBuffer)
+    }
+    
+    private func updatePreviewOverlayViewWithImageBuffer(_ imageBuffer: CVImageBuffer?) {
+        guard let imageBuffer = imageBuffer else {
+            return
+        }
+        let orientation: UIImage.Orientation = isUsingFrontCamera ? .leftMirrored : .right
+        let image = UIUtilities.createUIImage(from: imageBuffer, orientation: orientation)
+        previewOverlayView.image = image
+    }
+    
+    private func removeDetectionAnnotations() {
+        for annotationView in annotationOverlayView.subviews {
+            annotationView.removeFromSuperview()
+        }
+    }
+    
+    private func convertedPoints(
+        from points: [NSValue]?,
+        width: CGFloat,
+        height: CGFloat
+    ) -> [NSValue]? {
+        return points?.map {
+            let cgPointValue = $0.cgPointValue
+            let normalizedPoint = CGPoint(x: cgPointValue.x / width, y: cgPointValue.y / height)
+            let cgPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: normalizedPoint)
+            let value = NSValue(cgPoint: cgPoint)
+            return value
+        }
+    }
+    
+    private func normalizedPoint(
+        fromVisionPoint point: VisionPoint,
+        width: CGFloat,
+        height: CGFloat
+    ) -> CGPoint {
+        let cgPoint = CGPoint(x: point.x, y: point.y)
+        var normalizedPoint = CGPoint(x: cgPoint.x / width, y: cgPoint.y / height)
+        normalizedPoint = previewLayer.layerPointConverted(fromCaptureDevicePoint: normalizedPoint)
+        return normalizedPoint
+    }
+    
+    /// Resets any detector instances which use a conventional lifecycle paradigm. This method is
+    /// expected to be invoked on the AVCaptureOutput queue - the same queue on which detection is
+    /// run.
+    private func resetManagedLifecycleDetectors(activeDetector: Detector) {
+        if activeDetector == self.lastDetector {
+            // Same row as before, no need to reset any detectors.
+            return
+        }
+        // Clear the old detector, if applicable.
+        switch self.lastDetector {
+        case .pose:
+            self.poseDetector = nil
+            break
+        default:
+            break
+        }
+        // Initialize the new detector, if applicable.
+        switch activeDetector {
+        case .pose:
+            let options = activeDetector == .pose ? PoseDetectorOptions() : nil
+            self.poseDetector = PoseDetector.poseDetector(options: options!)
+            break
+        }
+
+        self.lastDetector = activeDetector
+    }
+    
+
+    
+    private func rotate(_ view: UIView, orientation: UIImage.Orientation) {
+        var degree: CGFloat = 0.0
+        switch orientation {
+        case .up, .upMirrored:
+            degree = 90.0
+        case .rightMirrored, .left:
+            degree = 180.0
+        case .down, .downMirrored:
+            degree = 270.0
+        case .leftMirrored, .right:
+            degree = 0.0
+        @unknown default:
+            degree = 90.0
+        }
+        view.transform = CGAffineTransform.init(rotationAngle: degree * 3.141592654 / 180)
+    }
+
+    func angle(
+        firstLandmark: PoseLandmark,
+        midLandmark: PoseLandmark,
+        lastLandmark: PoseLandmark
+    ) -> CGFloat {
+        let radians: CGFloat =
+            atan2(lastLandmark.position.y - midLandmark.position.y,
+                      lastLandmark.position.x - midLandmark.position.x) -
+              atan2(firstLandmark.position.y - midLandmark.position.y,
+                      firstLandmark.position.x - midLandmark.position.x)
+        var degrees = radians * 180.0 / .pi
+        degrees = abs(degrees) // Angle should never be negative
+        if degrees > 180.0 {
+            degrees = 360.0 - degrees // Always get the acute representation of the angle
+        }
+        return degrees
+    }
+    
+    
+}
+
+// MARK: AVCaptureVideoDataOutputSampleBufferDelegate
+
+extension PoseDetectViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection
+    ) {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            print("Failed to get image buffer from sample buffer.")
+            return
+        }
+        // Evaluate `self.currentDetector` once to ensure consistency throughout this method since it
+        // can be concurrently modified from the main thread.
+        let activeDetector = self.currentDetector
+        resetManagedLifecycleDetectors(activeDetector: activeDetector)
+        
+        
+        lastFrame = sampleBuffer
+        let visionImage = VisionImage(buffer: sampleBuffer)
+        let orientation = UIUtilities.imageOrientation(
+            fromDevicePosition: isUsingFrontCamera ? .front : .back
+        )
+        
+        visionImage.orientation = orientation
+        let imageWidth = CGFloat(CVPixelBufferGetWidth(imageBuffer))
+        let imageHeight = CGFloat(CVPixelBufferGetHeight(imageBuffer))
+        
+        switch activeDetector {
+        case .pose :
+            detectPose(in: visionImage, width: imageWidth, height: imageHeight)
+        }
+        
+    }
+}
+
+
+
+// MARK: - Constants
+
+public enum Detector: String {
+    case pose = "Pose Detection"
+}
+
+private enum Constant {
+    static let alertControllerTitle = "Vision Detectors"
+    static let alertControllerMessage = "Select a detector"
+    static let GymalertControllerTitle = "GymWorkType"
+    static let GymalertControllerMessage = "Select a GymWork"
+    static let cancelActionTitleText = "Cancel"
+    static let videoDataOutputQueueLabel = "com.google.mlkit.visiondetector.VideoDataOutputQueue"
+    static let sessionQueueLabel = "com.google.mlkit.visiondetector.SessionQueue"
+    static let noResultsMessage = "No Results"
+    static let localModelFile = (name: "bird", type: "tflite")
+    static let labelConfidenceThreshold = 0.75
+    static let smallDotRadius: CGFloat = 4.0
+    static let lineWidth: CGFloat = 3.0
+    static let originalScale: CGFloat = 1.0
+    static let padding: CGFloat = 10.0
+    static let resultsLabelHeight: CGFloat = 200.0
+    static let resultsLabelLines = 5
+    static let imageLabelResultFrameX = 0.4
+    static let imageLabelResultFrameY = 0.1
+    static let imageLabelResultFrameWidth = 0.5
+    static let imageLabelResultFrameHeight = 0.8
+    static let segmentationMaskAlpha: CGFloat = 0.5
+}
+
+
+
+
+
+
+
